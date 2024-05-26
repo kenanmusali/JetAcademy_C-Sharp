7a3ff1fab1b6d247d51eb247dc5646769f70b2cc
kenanmusali <textme0306@gmail.com>
2024-05-26 20:14:56 +0400

    First Task

---
+++ Task1C-Sharp/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task1C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Question 1
+            //Console.WriteLine(" ________________");
+            //Console.WriteLine("| Multiplication |");
+            //Console.WriteLine(" ----------------");
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Enter First Number");
+            //int firstNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Enter Second Number");
+            //int secondNum = int.Parse(Console.ReadLine());
+            //int amount = firstNum + secondNum;
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Answer");
+            //Console.WriteLine(amount);
+            #endregion
+
+            #region Question 2
+            //Console.WriteLine(" __________________");
+            //Console.WriteLine("| Simple Array Sum |");
+            //Console.WriteLine(" ------------------");
+            //Console.WriteLine(" ");
+            //int[] Num = { 6 };
+            //Console.WriteLine(Num[0]);
+            //int[] Num1 = { 1, 2, 3, 4, 10, 11 };
+            //int amount = Num1[0] + Num1[1] + Num1[2] + Num1[3] + Num1[4] + Num1[5];
+            //Console.WriteLine(amount);
+            #endregion
+
+            //ede Bilmedim Task 3-u
+            #region Question 3
+            //Console.WriteLine(" _________________");
+            //Console.WriteLine("| Reverse Integer |");
+            //Console.WriteLine(" -----------------");
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Enter Number");
+            //int firstNum = int.Parse(Console.ReadLine());
+            //int reversNum = firstNum 
+            //Console.WriteLine(" ");
+            #endregion 
+
+            //ede Bilmedim Task 4-u
+            #region Question 4
+            //Console.WriteLine(" ________________________");
+            //Console.WriteLine("| search insert position |");
+            //Console.WriteLine(" ------------------------");
+            //Console.WriteLine(" ");
+            //Console.WriteLine();
+            #endregion
+        }
+    }
+}
