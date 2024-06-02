90c03cc01f35427cfed6b40426566f187f29aa7a
kenanmusali <textme0306@gmail.com>
2024-06-02 22:03:48 +0400

    Second Task

---
+++ Task 2/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region RomanNumerals
+            //Ede Bilmedim
+            #endregion
+
+            #region DivideIntegers
+            Console.WriteLine("Type your dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type your divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int divide = dividend / divisor;
+            float divide1 = (float)dividend / divisor;
+            Console.WriteLine("Your Answer is: " + divide);
+            Console.WriteLine("Your Reminder is: " + divide1);
+            #endregion
+
+            #region ZigzagConversion
+            // Hecne basa dusmedim.
+            #endregion
+        }
+    }
+}
